8273cba119c3e3664b5b30d090634cb44f1b812d
WataruSuzuki <wataru0406@hotmail.co.jp>
2021-08-20 22:10:00 +0900

    Created xcode project

---
+++ Shared/JunkyardBarcodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  JunkyardBarcodeApp.swift
+//  Shared
+//
+//  Created by WataruSuzuki on 2022/01/31.
+//
+
+import SwiftUI
+
+@main
+struct JunkyardBarcodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
